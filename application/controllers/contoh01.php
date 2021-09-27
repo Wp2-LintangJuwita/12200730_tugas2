c953edcbefb5cace85c13bfae0daf5423f115d59
WP2-LintangJuwita <lintangsjuwita@gmail.com>
2021-09-27 12:41:54 +0700

    menambahkan framework

---
+++ application/controllers/contoh01.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public funtion index()
+	{
+		echo "<h1>perkenalkan</h1>";
+		echo"Nama saya Lintang Juwita Safitri
+			Saya tinggal di Gunung Kicau
+			Hobi saya fangirling no telp 082214522354";
+			}
+			}
+?>
